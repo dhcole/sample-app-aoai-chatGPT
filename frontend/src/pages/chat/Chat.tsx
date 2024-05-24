746b633811169adb334f486009287d66ac3b6c50
Dave Cole <170641+dhcole@users.noreply.github.com>
2024-05-24 13:57:04 -0400

    Allow for HTML in chat description

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -732,7 +732,9 @@ const Chat = () => {
               <Stack className={styles.chatEmptyState}>
                 <img src={ui?.chat_logo ? ui.chat_logo : Contoso} className={styles.chatIcon} aria-hidden="true" />
                 <h1 className={styles.chatEmptyStateTitle}>{ui?.chat_title}</h1>
-                <h2 className={styles.chatEmptyStateSubtitle}>{ui?.chat_description}</h2>
+                <h2
+                  className={styles.chatEmptyStateSubtitle}
+                  dangerouslySetInnerHTML={{ __html: ui?.chat_description }}></h2>
               </Stack>
             ) : (
               <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? '40px' : '0px' }} role="log">
